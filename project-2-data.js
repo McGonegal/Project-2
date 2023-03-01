60c8134f3370ba11d5c91cc37d771a98c0d2f0b1
krisMcGonegal <krismcgonegal@gmail.com>
2023-03-01 09:49:51 -0500

    Added trivia questions:

--- project-2-data.js
+++ project-2-data.js
@@ -1,118 +1,118 @@
 "triva" [
 		 {
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "During the American Prohibition, what name was given to the illegal taverns?",
+			"correctAnswer": "Speakeasies",
+			"answers": ['Prohibition Pubs', 'Speakeasies', 'Moonshines', 'Moonlighters']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What is the longest River in Ireland?",
+			"correctAnswer": "River Shannon",
+			"answers": ['River Liffey', 'River Barrow', 'River Nore', 'River Shannon']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What does Volkswagen mean?",
+			"correctAnswer": "People's car",
+			"answers": ['People\'s car', 'Family car', 'Powerful Car', 'Reliable Car']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who was the first gymnast to be awarded a 10.0 in the Olympic gammes?",
+			"correctAnswer": "Nadia Comaneci",
+			"answers": ['Beth Tweddle', 'Nadia Comaneci', 'Simone Biles', 'Larisa Latynina']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What is the collective noun for a group of owls?",
+			"correctAnswer": "A Parliament",
+			"answers": ['A Tower', 'A Murder', 'A Crash', 'A Parliament']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What element does the chemical symbol K stand for?",
+			"correctAnswer": "Potassium",
+			"answers": ['Lead', 'Potassium', 'Calcium', 'Iron']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which movie swept all the major oscar categories in 1992?",
+			"correctAnswer": "Silence of the Lambs",
+			"answers": ['Cape Fear', 'Thelma and Louise', 'Silence of the Lambs', 'Fried Green Tomatoes']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who wrote the book To Kill A Mockingbird?",
+			"correctAnswer": "Harper Lee",
+			"answers": ['Margaret Mitchell', 'Harper Lee', 'George Eliot', 'Carson MccCullers']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which astrological sign in represented by a scale?",
+			"correctAnswer": "Libra",
+			"answers": ['Libra', 'Aquarius', 'Capricorn', 'Virgo']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "In which country would you find the Hague?",
+			"correctAnswer": "the Netherlands",
+			"answers": ['Belgium', 'Denmark', 'Czech Republic', 'the Netherlands']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which greek mythological figure was cursed with prophetic visions no one would believe?",
+			"correctAnswer": "Cassandra",
+			"answers": ['Helen of Troy', 'Echo', 'Cassandra', 'The Oracle of Delphi']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which horror movie featured the tagline 'The Night He Came Home?",
+			"correctAnswer": "Halloween",
+			"answers": ['Friday the 13th', 'Halloween', 'Nightmare on Elm Street', 'My Bloody Valentine']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who was Henry the 8th's first wife?",
+			"correctAnswer": "Catherine of Aragon",
+			"answers": ['Catherine of Aragon', 'Anne Boleyn', 'Jane Seymour', 'Catherine Howard']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Where was the footballer Cristiano Ronaldo born?",
+			"correctAnswer": "Portugal",
+			"answers": ['Spain', 'Brazil', 'Uruguay', 'Portugal']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What country was not part of the Axis Powers during WWII?",
+			"correctAnswer": "the Soviet Union",
+			"answers": ['the Soviet Union', 'Germany', 'Italy', 'Japan']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which month is the aquamarine the birthstone of?",
+			"correctAnswer": "March",
+			"answers": ['January', 'April', 'March', 'June']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which band released Wonderwall in the 90s?",
+			"correctAnswer": "Oasis",
+			"answers": ['Verve', 'Oasis', 'Joy Division', 'Nirvana']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who was the second president of the United States?",
+			"correctAnswer": "John Adams",
+			"answers": ['Thomas Jefferson', 'James Madison', 'Benjamin Franklin', 'John Adams']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "On average, how many bones are in the adult human body?",
+			"correctAnswer": "206",
+			"answers": ['186', '206', '286', '306']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Which fruit is at the top of the Wimbledon gentleman's tropy?",
+			"correctAnswer": "a Pineapple",
+			"answers": ['a Pineapple', 'a Strawberry', 'a Banana', 'an Apple']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who painted the Girl with the Pearl Earring?",
+			"correctAnswer": "Johannes Vermeer",
+			"answers": ['Vincent Van Gogh', 'Leonardo da Vinci', 'Johannes Vermeer', 'Sandro Boticelli']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "What video game franchise features the characters Inky and Clyde?",
+			"correctAnswer": "Pacman",
+			"answers": ['Pokemon', 'Super Mario', 'Casltevania', 'Pacman']
 		},
 		{
-			"question": " ?",
-			"correctAnswer": "the answer",
-			"answers": ['an answer', 'an answer', 'an answer', 'an answer']
+			"question": "Who said 'the only thing I can't resist is temptation?'",
+			"correctAnswer": "Oscar Wilde",
+			"answers": ['Winston Churchill', 'Mark Twain', 'Oscar Wilde', 'Jesus']
 		}
 ]
 
