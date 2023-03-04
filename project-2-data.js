cd5942c1c93474e0bfb9b504189aa7632af804cd
krisMcGonegal <krismcgonegal@gmail.com>
2023-03-04 16:50:26 -0500

    changed trivia answers from array to object properties

--- project-2-data.js
+++ project-2-data.js
@@ -15,119 +15,188 @@ function addToDataBase(key, value){
 
 const triviaQuestions = [
 		 {
-			"question": "During the American Prohibition, what name was given to the illegal taverns?",
-			"correctAnswer": 1,
-			"answers": ['Prohibition Pubs', 'Speakeasies', 'Moonshines', 'Moonlighters']
+			question: "During the American Prohibition, what name was given to the illegal taverns?",
+			answer: 1,
+			choice1: 'Prohibition Pubs', 
+			choice2: 'Speakeasies', 
+			choice3: 'Moonshines', 
+			choice4: 'Moonlighters',
 		},
 		{
-			"question": "What is the longest River in Ireland?",
-			"correctAnswer": 3,
-			"answers": ['River Liffey', 'River Barrow', 'River Nore', 'River Shannon']
+			question: "What is the longest River in Ireland?",
+			answer: 4,
+			choice1: 'River Liffey', 
+			choice2: 'River Barrow', 
+			choice3: 'River Nore', 
+			choice4: 'River Shannon',
 		},
 		{
-			"question": "What does Volkswagen mean?",
-			"correctAnswer": "People's car",
-			"answers": ['People\'s car', 'Family car', 'Powerful Car', 'Reliable Car']
+			question: "What does Volkswagen mean?",
+			answer: 1,
+			choice1: 'People\'s car', 
+			choice2: 'Family car', 
+			choice3: 'Powerful Car', 
+			choice4: 'Reliable Car',
 		},
 		{
-			"question": "Who was the first gymnast to be awarded a 10.0 in the Olympic gammes?",
-			"correctAnswer": "Nadia Comaneci",
-			"answers": ['Beth Tweddle', 'Nadia Comaneci', 'Simone Biles', 'Larisa Latynina']
+			question: "Who was the first gymnast to be awarded a 10.0 in the Olympic gammes?",
+			answer: 2,
+			choice1: 'Beth Tweddle', 
+			choice2: 'Nadia Comaneci', 
+			choice3: 'Simone Biles', 
+			choice4: 'Larisa Latynina',
 		},
 		{
-			"question": "What is the collective noun for a group of owls?",
-			"correctAnswer": "A Parliament",
-			"answers": ['A Tower', 'A Murder', 'A Crash', 'A Parliament']
+			question: "What is the collective noun for a group of owls?",
+			answer: 4,
+			choice1: 'A Tower',
+			choice2: 'A Murder', 
+			choice3:'A Crash', 
+			choice4:'A Parliament',
 		},
 		{
-			"question": "What element does the chemical symbol K stand for?",
-			"correctAnswer": "Potassium",
-			"answers": ['Lead', 'Potassium', 'Calcium', 'Iron']
+			question: "What element does the chemical symbol K stand for?",
+			answer: 2,
+			choice1:'Lead', 
+			choice2:'Potassium', 
+			choice3:'Calcium', 
+			choice4: 'Iron',
 		},
 		{
-			"question": "Which movie swept all the major oscar categories in 1992?",
-			"correctAnswer": "Silence of the Lambs",
-			"answers": ['Cape Fear', 'Thelma and Louise', 'Silence of the Lambs', 'Fried Green Tomatoes']
+			question: "Which movie swept all the major oscar categories in 1992?",
+			answer: 3,
+			choice1: 'Cape Fear', 
+			choice2: 'Thelma and Louise', 
+			choice3: 'Silence of the Lambs', 
+			choice4: 'Fried Green Tomatoes',
 		},
 		{
-			"question": "Who wrote the book To Kill A Mockingbird?",
-			"correctAnswer": "Harper Lee",
-			"answers": ['Margaret Mitchell', 'Harper Lee', 'George Eliot', 'Carson MccCullers']
+			question: "Who wrote the book To Kill A Mockingbird?",
+			answer: 2,
+			choice1: 'Margaret Mitchell', 
+			choice2: 'Harper Lee', 
+			choice3: 'George Eliot', 
+			choice4: 'Carson MccCullers',
 		},
 		{
-			"question": "Which astrological sign in represented by a scale?",
-			"correctAnswer": "Libra",
-			"answers": ['Libra', 'Aquarius', 'Capricorn', 'Virgo']
+			question: "Which astrological sign in represented by a scale?",
+			answer: 1,
+			choice1: 'Libra', 
+			choice2: 'Aquarius', 
+			choice3: 'Capricorn', 
+			choice4: 'Virgo',
 		},
 		{
-			"question": "In which country would you find the Hague?",
-			"correctAnswer": "the Netherlands",
-			"answers": ['Belgium', 'Denmark', 'Czech Republic', 'the Netherlands']
+			question: "In which country would you find the Hague?",
+			answer: 4,
+			choice1: 'Belgium', 
+			choice2: 'Denmark', 
+			choice3: 'Czech Republic', 
+			choice4: 'the Netherlands',
 		},
 		{
-			"question": "Which greek mythological figure was cursed with prophetic visions no one would believe?",
-			"correctAnswer": "Cassandra",
-			"answers": ['Helen of Troy', 'Echo', 'Cassandra', 'The Oracle of Delphi']
+			question: "Which greek mythological figure was cursed with prophetic visions no one would believe?",
+			answer: 3,
+			choice1: 'Helen of Troy', 
+			choice2: 'Echo', 
+			choice3: 'Cassandra', 
+			choice4: 'The Oracle of Delphi',
 		},
 		{
-			"question": "Which horror movie featured the tagline 'The Night He Came Home?",
-			"correctAnswer": "Halloween",
-			"answers": ['Friday the 13th', 'Halloween', 'Nightmare on Elm Street', 'My Bloody Valentine']
+			question: "Which horror movie featured the tagline 'The Night He Came Home?",
+			answer: 2,
+			choice1: 'Friday the 13th', 
+			choice2: 'Halloween', 
+			choice3: 'Nightmare on Elm Street', 
+			choice4: 'My Bloody Valentine',
 		},
 		{
-			"question": "Who was Henry the 8th's first wife?",
-			"correctAnswer": "Catherine of Aragon",
-			"answers": ['Catherine of Aragon', 'Anne Boleyn', 'Jane Seymour', 'Catherine Howard']
+			question: "Who was Henry the 8th's first wife?",
+			answer: 1,
+			choice1: 'Catherine of Aragon', 
+			choice2: 'Anne Boleyn', 
+			choice3: 'Jane Seymour', 
+			choice4: 'Catherine Howard',
 		},
 		{
-			"question": "Where was the footballer Cristiano Ronaldo born?",
-			"correctAnswer": "Portugal",
-			"answers": ['Spain', 'Brazil', 'Uruguay', 'Portugal']
+			question: "Where was the footballer Cristiano Ronaldo born?",
+			answer: 4,
+			choice1: 'Spain', 
+			choice2: 'Brazil', 
+			choice3: 'Uruguay', 
+			choice4: 'Portugal',
 		},
 		{
-			"question": "What country was not part of the Axis Powers during WWII?",
-			"correctAnswer": "the Soviet Union",
-			"answers": ['the Soviet Union', 'Germany', 'Italy', 'Japan']
+			question: "What country was not part of the Axis Powers during WWII?",
+			answer: 1,
+			choice1: 'the Soviet Union', 
+			choice2: 'Germany', 
+			choice3: 'Italy', 
+			choice4: 'Japan',
 		},
 		{
-			"question": "Which month is the aquamarine the birthstone of?",
-			"correctAnswer": "March",
-			"answers": ['January', 'April', 'March', 'June']
+			question: "Which month is the aquamarine the birthstone of?",
+			answer: 3,
+			choice1: 'January', 
+			choice2: 'April', 
+			choice3: 'March', 
+			choice4: 'June',
 		},
 		{
-			"question": "Which band released Wonderwall in the 90s?",
-			"correctAnswer": "Oasis",
-			"answers": ['Verve', 'Oasis', 'Joy Division', 'Nirvana']
+			question: "Which band released Wonderwall in the 90s?",
+			answer: 2,
+			choice1: 'Verve', 
+			choice2: 'Oasis', 
+			choice3: 'Joy Division', 
+			choice4: 'Nirvana',
 		},
 		{
-			"question": "Who was the second president of the United States?",
-			"correctAnswer": "John Adams",
-			"answers": ['Thomas Jefferson', 'James Madison', 'Benjamin Franklin', 'John Adams']
+			question: "Who was the second president of the United States?",
+			answer: 4,
+			choice1: 'Thomas Jefferson', 
+			choice2: 'James Madison', 
+			choice3: 'Benjamin Franklin', 
+			choice4: 'John Adams',
 		},
 		{
-			"question": "On average, how many bones are in the adult human body?",
-			"correctAnswer": "206",
-			"answers": ['186', '206', '286', '306']
+			question: "On average, how many bones are in the adult human body?",
+			answer: 2,
+			choice1: '186', 
+			choice2: '206', 
+			choice3: '286', 
+			choice4: '306',
 		},
 		{
-			"question": "Which fruit is at the top of the Wimbledon gentleman's tropy?",
-			"correctAnswer": "a Pineapple",
-			"answers": ['a Pineapple', 'a Strawberry', 'a Banana', 'an Apple']
+			question: "Which fruit is at the top of the Wimbledon gentleman's tropy?",
+			answer: 1,
+			choice1: 'a Pineapple', 
+			choice2: 'a Strawberry', 
+			choice3: 'a Banana', 
+			choice4: 'an Apple',
 		},
 		{
-			"question": "Who painted the Girl with the Pearl Earring?",
-			"correctAnswer": "Johannes Vermeer",
-			"answers": ['Vincent Van Gogh', 'Leonardo da Vinci', 'Johannes Vermeer', 'Sandro Boticelli']
+			question: "Who painted the Girl with the Pearl Earring?",
+			answer: 3,
+			choice1: 'Vincent Van Gogh', 
+			choice2: 'Leonardo da Vinci', 
+			choice3: 'Johannes Vermeer', 
+			choice4: 'Sandro Boticelli',
 		},
 		{
-			"question": "What video game franchise features the characters Inky and Clyde?",
-			"correctAnswer": "Pacman",
-			"answers": ['Pokemon', 'Super Mario', 'Casltevania', 'Pacman']
+			question: "What video game franchise features the characters Inky and Clyde?",
+			answer: 4,
+			choice1: 'Pokemon', 
+			choice2: 'Super Mario', 
+			choice3: 'Casltevania', 
+			choice4: 'Pacman',
 		},
 		{
-			"question": "Who said 'the only thing I can't resist is temptation?'",
-			"correctAnswer": "Oscar Wilde",
-			"answers": ['Winston Churchill', 'Mark Twain', 'Oscar Wilde', 'Jesus']
+			question: "Who said 'the only thing I can't resist is temptation?'",
+			answer: 3,
+			choice1: 'Winston Churchill', 
+			choice2: 'Mark Twain', 
+			choice3: 'Oscar Wilde', 
+			choice4: 'Jesus',
 		}
 ]
 
